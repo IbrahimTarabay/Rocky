1820dbeb22d8800a825b6f4ea00a621d17fc1136
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-08-05 00:53:09 +0200

    validation-edit-delete to ApplicationType

--- Controllers/ApplicationTypeController.cs
+++ Controllers/ApplicationTypeController.cs
@@ -8,28 +8,96 @@ using Rocky.Models;
 
 namespace Rocky.Controllers
 {
-    public class ApplicationTypeController : Controller{
+    public class ApplicationTypeController : Controller
+    {
         private readonly ApplicationDbContext _db;
 
-        public ApplicationTypeController(ApplicationDbContext db) {
+        public ApplicationTypeController(ApplicationDbContext db)
+        {
             _db = db;
         }
-        public IActionResult Index(){
+        public IActionResult Index()
+        {
             IEnumerable<ApplicationType> objList = _db.ApplicationType;
             return View(objList);
         }
 
         //GET - CREATE
-        public IActionResult Create(){   
+        public IActionResult Create()
+        {
             return View();
         }
 
         //POST - CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {//this define if rules you write in ApplicationType model is applied 
+                _db.ApplicationType.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
 
-        public IActionResult Create(ApplicationType obj) {
-            _db.ApplicationType.Add(obj);
+        //GET - Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);//Find() work only with primary key
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {//this define if rules you write in category model is applied 
+                _db.ApplicationType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET - Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);//Find() work only with primary key
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(ApplicationType obj)
+        {
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.ApplicationType.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
--- Models/ApplicationType.cs
+++ Models/ApplicationType.cs
@@ -9,6 +9,7 @@ namespace Rocky.Models
 {
     public class ApplicationType{
       public int Id { get; set; }
+      [Required]
       public string Name { get; set; }
     }
 }
