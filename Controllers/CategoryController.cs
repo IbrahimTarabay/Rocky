8a6654f4e5bc1f4bf9489b3397c3cea2239061ff
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-08-04 22:34:03 +0200

    First Commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller{
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) {
+            _db = db;
+        }
+        public IActionResult Index(){
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+
+        //GET - CREATE
+        public IActionResult Create(){   
+            return View();
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create(Category obj) {
+            if (ModelState.IsValid) {//this define if rules you write in category model is applied 
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id) {
+            if (id==null || id==0) {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);//Find() work only with primary key
+            if (obj==null) {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+    }
+}
