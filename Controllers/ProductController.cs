d79f3820ad8b9b2eb75cc372f68af092b294c286
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-08-06 01:18:46 +0200

    View Model

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Rocky.Data;
 using Rocky.Models;
+using Rocky.Models.ViewModels;
 
 namespace Rocky.Controllers
 {
@@ -26,29 +27,43 @@ namespace Rocky.Controllers
         }
 
         //GET - Upsert
-        public IActionResult Upsert(int? id){
+        public IActionResult Upsert(int? id) {
 
             //so we are retrieving all of the categories from the database,
             //but we are converting them to a SelectListItem so that we can have enumerable object and can display them in a dropdown
-            IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(i => new SelectListItem
+
+            //IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(i => new SelectListItem
+            //{
+            //Text = i.Name,
+            //Value = i.Id.ToString()
+            //});
+
+            //ViewBag.CategoryDropDown = CategoryDropDown;
+            //ViewData["CategoryDropDown"] = CategoryDropDown;
+
+            //Product product = new Product();
+
+            ProductVM productVM = new ProductVM()
             {
-                Text = i.Name,
-                Value = i.Id.ToString()
-            });
+                Product = new Product(),
+                CategorySelectList = _db.Category.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
 
-            ViewBag.CategoryDropDown = CategoryDropDown;
+                })
+            };
 
-            Product product = new Product();
             if (id == null)
             {
                 //This is for create
-                return View(product);
+                return View(productVM);
             }else {
-                product = _db.Product.Find(id);
-                if (product == null) {
+                productVM.Product = _db.Product.Find(id);
+                if (productVM.Product == null) {
                     return NotFound();
                 }
-                return View(product);
+                return View(productVM);
             }
         }
 
