49b0c0260e354bb4447d562c677d84ae3c968596
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-08-07 17:58:33 +0200

    Eager Loading

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -24,12 +24,13 @@ namespace Rocky.Controllers
             _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index(){
-            IEnumerable<Product> objList = _db.Product;
+            IEnumerable<Product> objList = _db.Product.Include(u => u.Category).Include(u=>u.ApplicationType); ;
 
-            foreach (var obj in objList) {
+            /*foreach (var obj in objList) {
                 obj.Category = _db.Category.FirstOrDefault(u => u.Id == obj.CategoryId);
                 obj.ApplicationType = _db.ApplicationType.FirstOrDefault(u => u.Id == obj.ApplicationTypeId);
-            }
+            }*/
+
             return View(objList);
         }
 
