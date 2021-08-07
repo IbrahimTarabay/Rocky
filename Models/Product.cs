07f4a7922402d4d82b1105e95caf4ed4d8aea9ee
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-08-07 18:07:13 +0200

    Modify Product Model

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,21 +12,26 @@ namespace Rocky.Models
       [Key]
       public int Id { get; set; }
       public string Name { get; set; }
+      public string ShortDesc { get; set; }
       public string Description { get; set; }
+
       [Range(1, int.MaxValue)]
       public double Price { get; set; }
       public string Image { get; set; }
+
       [Display(Name = "Category Type")]
       public int CategoryId { get; set; }
+
       [ForeignKey("CategoryId")]
       public virtual Category Category { get; set; }
       //EF automatically adds a mapping between product and category
       //it will also create a category id column which will be the foreign key relation between both tables
 
-        [Display(Name = "Application Type")]
-        public int ApplicationTypeId { get; set; }
-        [ForeignKey("ApplicationTypeId")]
-        public virtual ApplicationType ApplicationType { get; set; }
+      [Display(Name = "Application Type")]
+      public int ApplicationTypeId { get; set; }
+
+      [ForeignKey("ApplicationTypeId")]
+      public virtual ApplicationType ApplicationType { get; set; }
 
     }
 }
