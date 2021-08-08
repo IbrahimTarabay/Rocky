270c64f18a7b60b7cccc06a471ed87cb0de46342
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-08-08 13:38:20 +0200

    Add to Session

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Rocky.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Rocky.Data;
-using Rocky.Models.ViewModels;
 using System.Globalization;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Models.ViewModels;
+using Rocky.Utility;
 
 namespace Rocky.Controllers
 {
@@ -46,6 +48,22 @@ namespace Rocky.Controllers
             };
             
             return View(DetailsVM);
+        }
+
+        [HttpPost,ActionName("Details")]
+        public IActionResult DetailsPost(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+
+            //retrieve shopping cart list before adding new one
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart)!=null 
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0) {
+
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+            shoppingCartList.Add(new ShoppingCart { ProductId = id });
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));//magic strings
         }
 
         public IActionResult Privacy()
