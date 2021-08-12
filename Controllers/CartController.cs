0cbd2467e4455cb13952a4c33ecdf3b3c685636f
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-08-12 17:09:24 +0200

    Summary Get Action

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Rocky.Data;
 using Rocky.Models;
 using Rocky.Utility;
+using System.Security.Claims;
+using Rocky.Models.ViewModels;
 
 namespace Rocky.Controllers
 {   
@@ -14,6 +16,8 @@ namespace Rocky.Controllers
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
 
         public CartController(ApplicationDbContext db) {
             _db = db;
@@ -32,6 +36,37 @@ namespace Rocky.Controllers
             IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
 
             return View(prodList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary() {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            //var userId = User.FindFirstValue(ClaimTypes.Name);
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                //session exists
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value)
+            };
+            return View(ProductUserVM);
         }
 
         public IActionResult Remove(int id)
