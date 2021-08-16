2bf3955f7bafbafa610dcf673d36bf5112f8d1c1
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-08-16 21:00:36 +0200

    new structure

---
+++ Rocky_Models/ViewModels/DetailsVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Rocky_Models.ViewModels
+{
+    public class DetailsVM{
+        public DetailsVM() {
+            Product = new Product();
+        }
+
+        public Product Product { get; set; }
+        public bool ExistsInCart { get; set; }
+        public NumberFormatInfo myNumberFormatInfo { get; set; }
+    }
+}
---
+++ Rocky_Utility/EmailSender.cs
@@ -0,0 +1,70 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Rocky_Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+        public MailJetSettings _mailJetSettings { get; set; }
+
+        public EmailSender(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email,subject,htmlMessage);
+        }
+
+        public async Task Execute(string email, string subject, string body) {
+            _mailJetSettings = _configuration.GetSection("MailJet").Get<MailJetSettings>();
+
+            MailjetClient client = new MailjetClient(_mailJetSettings.ApiKey, _mailJetSettings.SecretKey);
+
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = SendV31.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+     new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", "ibrahimesalah69@gmail.com"},
+        {"Name", "Ibrahim"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          email
+         }, {
+          "Name",
+          "Ibrahim"
+         }
+        }
+       }
+      }, {
+       "Subject",
+       subject
+      },
+      {
+       "HTMLPart",
+        body
+       }
+     }
+             });
+        await client.PostAsync(request);
+        }
+    }
+}
