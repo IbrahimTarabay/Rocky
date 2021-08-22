b343992c2e22a797507206ddac2e3fde2e7936cf
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-08-22 11:53:52 +0200

    Toastr Notifications Partial View

--- Rocky_Utility/WC.cs
+++ Rocky_Utility/WC.cs
@@ -17,5 +17,8 @@ namespace Rocky_Utility
 
       public const string CategoryName = "Category";
       public const string ApplicationTypeName = "ApplicationType";
+
+      public const string Success = "Success";
+      public const string Error = "Error";
     }
 }
