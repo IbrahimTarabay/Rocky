21607e79c88b80cf012f4cd23622340cc8a1a7f5
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-08-25 19:50:00 +0200

    More fields on Order Summary

--- Rocky_Models/ApplicationUser.cs
+++ Rocky_Models/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace Rocky_Models
@@ -9,5 +10,14 @@ namespace Rocky_Models
     public class ApplicationUser : IdentityUser
     {
         public string FullName { get; set; }
+
+        [NotMapped]//not saved in the database
+        public string StreetAddress { get; set; }
+        [NotMapped]
+        public string City { get; set; }
+        [NotMapped]
+        public string State { get; set; }
+        [NotMapped]
+        public string PostalCode { get; set; }
     }
 }
