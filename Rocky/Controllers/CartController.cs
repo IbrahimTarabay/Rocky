72dbc148d1eecf28535192bc3ebe9209809cd915
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-09-03 14:52:22 +0200

    Create Order

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -25,14 +25,16 @@ namespace Rocky.Controllers
         private readonly IProductRepository _prodRepo;
         private readonly IInquiryHeaderRepository _inqHRepo;
         private readonly IInquiryDetailRepository _inqDRepo;
-
+        private readonly IOrderHeaderRepository _orderHRepo;
+        private readonly IOrderDetailRepository _orderDRepo;
 
         [BindProperty]
         public ProductUserVM ProductUserVM { get; set; }
 
         public CartController(IWebHostEnvironment webHostEnvironment,
             IEmailSender emailSender,IApplicationUserRepository userRepo,IProductRepository prodRepo,
-            IInquiryHeaderRepository inqHRepo, IInquiryDetailRepository inqDRepo) {
+            IInquiryHeaderRepository inqHRepo, IInquiryDetailRepository inqDRepo,
+            IOrderHeaderRepository orderHRepo, IOrderDetailRepository orderDRepo) {
             
             _webHostEnvironment = webHostEnvironment;
             _emailSender = emailSender;
@@ -40,6 +42,8 @@ namespace Rocky.Controllers
             _prodRepo = prodRepo;
             _inqHRepo = inqHRepo;
             _inqDRepo = inqDRepo;
+            _orderDRepo = orderDRepo;
+            _orderHRepo = orderHRepo;
         }
         public IActionResult Index()
         {
@@ -104,6 +108,7 @@ namespace Rocky.Controllers
             }
             else
             {
+                //When you need to integrate authorizing the user to perform some activity
                 var claimsIdentity = (ClaimsIdentity)User.Identity;
                 var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
                 //var userId = User.FindFirstValue(ClaimTypes.Name);
@@ -142,64 +147,113 @@ namespace Rocky.Controllers
         [ActionName("Summary")]
         public async Task<IActionResult> SummaryPost(ProductUserVM ProductUserVM)//you can put no parameter since it's already binded above
         {
+
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-            
-            var PathToTemplate = _webHostEnvironment.WebRootPath + Path.DirectorySeparatorChar.ToString()
-                + "templates" + Path.DirectorySeparatorChar.ToString() + "Inquiry.html";
 
-            var subject = "New Inquiry";
-            string HtmlBody = "";
-            using (StreamReader sr = System.IO.File.OpenText(PathToTemplate)) {
-                HtmlBody = sr.ReadToEnd();
+
+            if (User.IsInRole(WC.AdminRole)) {
+
+                //we need to create an order
+                var orderTotal = 0.0;
+                foreach (Product prod in ProductUserVM.ProductList)
+                {
+                    orderTotal += prod.Price * prod.TempSqFt;
+                }
+                OrderHeader orderHeader = new OrderHeader()
+                {
+                    CreatedByUserId = claim.Value,
+                    FinalOrderTotal = orderTotal,
+                    City = ProductUserVM.ApplicationUser.City,
+                    StreetAddress = ProductUserVM.ApplicationUser.StreetAddress,
+                    State = ProductUserVM.ApplicationUser.State,
+                    PostalCode = ProductUserVM.ApplicationUser.PostalCode,
+                    FullName = ProductUserVM.ApplicationUser.FullName,
+                    Email = ProductUserVM.ApplicationUser.Email,
+                    PhoneNumber = ProductUserVM.ApplicationUser.PhoneNumber,
+                    OrderDate = DateTime.Now,
+                    OrderStatus = WC.StatusPending
+                };
+                _orderHRepo.Add(orderHeader);
+                _orderHRepo.Save();
+
+                foreach (var prod in ProductUserVM.ProductList)
+                {
+                    OrderDetail orderDetail = new OrderDetail()
+                    {
+                        OrderHeaderId = orderHeader.Id,
+                        PricePerSqFt = prod.Price,
+                        Sqft = prod.TempSqFt,
+                        ProductId = prod.Id
+                    };
+                    _orderDRepo.Add(orderDetail);
+
+                }
+                _orderDRepo.Save();
+                return RedirectToAction(nameof(InquiryConfirmation), new { id = orderHeader.Id });
             }
-            //Name: {0}
-            //Email: {1}
-            //Phone: {2}
-            //Products: {3}
+            else
+            {
+                //we need to create an inquiry
+                var PathToTemplate = _webHostEnvironment.WebRootPath + Path.DirectorySeparatorChar.ToString()
+               + "templates" + Path.DirectorySeparatorChar.ToString() +
+               "Inquiry.html";
 
-            //we use StringBuilder because we want product list shown as html
-            StringBuilder productListSB = new StringBuilder();
-            foreach (var prod in ProductUserVM.ProductList) {
-                productListSB.Append($" - Name: {prod.Name} <span style='font-size:14px;'>(ID: {prod.Id})</span><br />");
-            }
+                var subject = "New Inquiry";
+                string HtmlBody = "";
+                using (StreamReader sr = System.IO.File.OpenText(PathToTemplate))
+                {
+                    HtmlBody = sr.ReadToEnd();
+                }
+                //Name: { 0}
+                //Email: { 1}
+                //Phone: { 2}
+                //Products: {3}
 
-            string messageBody = string.Format(HtmlBody,
+                StringBuilder productListSB = new StringBuilder();
+                foreach (var prod in ProductUserVM.ProductList)
+                {
+                    productListSB.Append($" - Name: { prod.Name} <span style='font-size:14px;'> (ID: {prod.Id})</span><br />");
+                }
+
+                string messageBody = string.Format(HtmlBody,
                 ProductUserVM.ApplicationUser.FullName,
                 ProductUserVM.ApplicationUser.Email,
                 ProductUserVM.ApplicationUser.PhoneNumber,
-                productListSB.ToString()
-                );
+                productListSB.ToString());
 
-            await _emailSender.SendEmailAsync(WC.EmailAdmin, subject,messageBody);
 
-            InquiryHeader inquiryHeader = new InquiryHeader()
-            {
+                await _emailSender.SendEmailAsync(WC.EmailAdmin, subject, messageBody);
 
-                ApplicationUserId = claim.Value,
-                FullName = ProductUserVM.ApplicationUser.FullName,
-                Email = ProductUserVM.ApplicationUser.Email,
-                PhoneNumber = ProductUserVM.ApplicationUser.PhoneNumber,
-                InquiryDate = DateTime.Now
-            };
-
-            _inqHRepo.Add(inquiryHeader);
-            _inqHRepo.Save();
-
-            foreach (var prod in ProductUserVM.ProductList) {
-                InquiryDetail inquiryDetail = new InquiryDetail()
-                {
-                    InquiryHeaderId = inquiryHeader.Id,
-                    ProductId = prod.Id
+                InquiryHeader inquiryHeader = new InquiryHeader() { 
+                    ApplicationUserId = claim.Value,
+                    FullName = ProductUserVM.ApplicationUser.FullName,
+                    Email = ProductUserVM.ApplicationUser.Email,
+                    PhoneNumber = ProductUserVM.ApplicationUser.PhoneNumber,
+                    InquiryDate = DateTime.Now
                 };
-                _inqDRepo.Add(inquiryDetail);
-            }
-            //to not make call for each loop
-            _inqDRepo.Save();
-            TempData[WC.Success] = "Inquiry submitted successfully";
 
+                _inqHRepo.Add(inquiryHeader);
+                _inqHRepo.Save();
+
+                foreach (var prod in ProductUserVM.ProductList)
+                {
+                    InquiryDetail inquiryDetail = new InquiryDetail()
+                    {
+                        InquiryHeaderId = inquiryHeader.Id,
+                        ProductId = prod.Id,
+
+                    };
+                    _inqDRepo.Add(inquiryDetail);
+
+                }
+                _inqDRepo.Save();
+                TempData[WC.Success] = "Inquiry submitted successfully";
+            }
             return RedirectToAction(nameof(InquiryConfirmation));
         }
+
+
 
         public IActionResult InquiryConfirmation()
         {
