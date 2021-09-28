fe973d12dfc398c4e9033668ecd6651609ac54e3
IbrahimTarabay <ibrahimesalah69@gmail.com>
2021-09-28 20:45:14 +0200

    Seed Database-2

--- Rocky/Startup.cs
+++ Rocky/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Rocky_DataAccess;
+using Rocky_DataAccess.Initializer;
 using Rocky_DataAccess.Repository;
 using Rocky_DataAccess.Repository.IRepository;
 using Rocky_Utility;
@@ -61,7 +62,7 @@ namespace Rocky
             services.AddScoped<IApplicationUserRepository, ApplicationUserRepository>();
             services.AddScoped<IOrderHeaderRepository, OrderHeaderRepository>();
             services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
-
+            services.AddScoped<IDbInitializer, DbInitializer>();
 
             services.AddAuthentication().AddFacebook(Options =>
             {
@@ -73,7 +74,7 @@ namespace Rocky
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDbInitializer dbInitializer)
         {
             if (env.IsDevelopment())
             {
@@ -91,6 +92,7 @@ namespace Rocky
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
+            dbInitializer.Initialize();
             app.UseSession();
             app.UseEndpoints(endpoints =>
             {
--- Rocky_DataAccess/Initializer/DbInitializer.cs
+++ Rocky_DataAccess/Initializer/DbInitializer.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Rocky_Models;
+using Rocky_Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Rocky_DataAccess.Initializer
 {
     public class DbInitializer : IDbInitializer
@@ -12,7 +14,6 @@ namespace Rocky_DataAccess.Initializer
         private readonly ApplicationDbContext _db;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
-
         public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
@@ -22,7 +23,42 @@ namespace Rocky_DataAccess.Initializer
 
         public void Initialize()
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+            if (!_roleManager.RoleExistsAsync(WC.AdminRole).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(WC.AdminRole)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WC.CustomerRole)).GetAwaiter().GetResult();
+            }
+            else
+            {
+                return;
+            }
+
+            _userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = "admin",
+                Email = "admin@admin.com",
+                EmailConfirmed = true,
+                FullName = "Admin Tester",
+                PhoneNumber = "111111111111"
+            }, "Admin123*").GetAwaiter().GetResult();
+
+            ApplicationUser user = _db.ApplicationUser.FirstOrDefault(u => u.Email == "admin@admin.com");
+            _userManager.AddToRoleAsync(user, WC.AdminRole).GetAwaiter().GetResult();
+
+
         }
     }
 }
